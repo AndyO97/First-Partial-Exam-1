d6bc43ee0f922ba18fc3ba5e25ef239469f80716
AndyO97 <47606055+AndyO97@users.noreply.github.com>
2020-03-27 09:58:06 -0600

    Add files via upload

---
+++ Coding-Challenge-3/js/index.js
@@ -0,0 +1,10 @@
+
+
+let Fizzbutton = document.getElementById(submitBtn);    
+
+Fizzbutton.addEventListener("Submit", (event)=>{
+
+    if(0<==Number(event.target.innerText))
+    console(event.target.innerText);
+})
+
